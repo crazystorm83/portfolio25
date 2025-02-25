7076b198ca6e92dee29f5b75faad10e01d92794a
crazystorm83 <crazystorm83@gmail.com>
2025-02-26 04:08:22 +0900

    fix: flow

--- src/documents/AST/FlowDefinition.ts
+++ src/documents/AST/FlowDefinition.ts
@@ -1,16 +1,17 @@
-아래는 typescript 로 정의된 데이터를 흐름도로 만들어줘.
+//아래는 typescript 로 정의된 데이터를 흐름도로 만들어줘.
 
 enum EN_FLOW_AST {
     Program = 'program',
-    
+
     MetadataStatement = 'metadatastatement',
-    
+
     IfStatement = 'ifstatement',
     CombinationStatement = 'combinationstatement',
     ResultStatement = 'resultstatement',
-    
-    CallExpression: 'callexpression',
-    
+    BinaryExpression = 'binaryexpression',
+
+    CallExpression = 'callexpression',
+
     ArrayExpression = 'arrayexpression',
     ObjectExpression = 'objectexpression',
     /*
@@ -18,108 +19,134 @@ enum EN_FLOW_AST {
      */
     PropertyExpression = 'propertyexpression',
     LiteralExpression = 'literalexpression',
-    
+
     OperatorExpression = 'operatorexpression',
-    BracketExpression = 'bracketexpression'
+    BracketExpression = 'bracketexpression',
 }
 
 enum EN_BRACKET_TYPE {
     Round = 'round',
     Square = 'square',
-    Angle = 'angle'
+    Angle = 'angle',
 }
 
-const EN_FLOW_PROVIDERS {
+const EN_FLOW_PROVIDERS = {
     decimal: 'decimal' as const,
     code: 'code' as const,
     string: 'string' as const,
     date: 'date' as const,
     datetime: 'datetime' as const,
-} as const
+} as const;
 
-type EN_FLOW_PROVIDER = typeof EN_FLOW_PROVIDERS[keyof typeof EN_FLOW_PROVIDERS];
+type EN_FLOW_PROVIDER =
+    (typeof EN_FLOW_PROVIDERS)[keyof typeof EN_FLOW_PROVIDERS];
 
-const EN_FLOW_UI_PROVIDERS {
-    any: 'any' as const
-} as const
+const EN_FLOW_UI_PROVIDERS = {
+    any: 'any' as const,
+} as const;
 
-type EN_FLOW_UI_PROVIDER = typeof EN_FLOW_UI_PROVIDERS[keyof typeof EN_FLOW_UI_PROVIDERS] | EN_FLOW_PROVIDER
+type EN_FLOW_UI_PROVIDER =
+    | (typeof EN_FLOW_UI_PROVIDERS)[keyof typeof EN_FLOW_UI_PROVIDERS]
+    | EN_FLOW_PROVIDER;
 
 const EN_FLOW_EQUAL_FUNCTIONS = {
-    eq:'eq' as const
+    eq: 'eq' as const,
 } as const;
 
 const EN_FLOW_NOT_EQAUL_FUNCTIONS = {
-    neq: 'neq' as const
+    neq: 'neq' as const,
 } as const;
 
-type EN_FLOW_EQUAL_FUNCTION = typeof EN_FLOW_EQUAL_FUNCTIONS[keyof typeof EN_FLOW_EQUAL_FUNCTIONS];
-type EN_FLOW_NOT_EQAUL_FUNCTION = typeof EN_FLOW_NOT_EQAUL_FUNCTIONS[keyof typeof EN_FLOW_NOT_EQAUL_FUNCTIONS];
+type EN_FLOW_EQUAL_FUNCTION =
+    (typeof EN_FLOW_EQUAL_FUNCTIONS)[keyof typeof EN_FLOW_EQUAL_FUNCTIONS];
+type EN_FLOW_NOT_EQAUL_FUNCTION =
+    (typeof EN_FLOW_NOT_EQAUL_FUNCTIONS)[keyof typeof EN_FLOW_NOT_EQAUL_FUNCTIONS];
 
-type EN_FLOW_DECIMAL_FUNCTION = EN_FLOW_EQUAL_FUNCTION | EN_FLOW_NOT_EQAUL_FUNCTION;
-type EN_FLOW_CODE_FUNCTION = EN_FLOW_EQUAL_FUNCTION | EN_FLOW_NOT_EQAUL_FUNCTION;
-type EN_FLOW_STRING_FUNCTION = EN_FLOW_EQUAL_FUNCTION | EN_FLOW_NOT_EQAUL_FUNCTION;
-type EN_FLOW_DATE_FUNCTION = EN_FLOW_EQUAL_FUNCTION | EN_FLOW_NOT_EQAUL_FUNCTION;
-type EN_FLOW_DATETIME_FUNCTION = EN_FLOW_EQUAL_FUNCTION | EN_FLOW_NOT_EQAUL_FUNCTION;
+type EN_FLOW_DECIMAL_FUNCTION =
+    | EN_FLOW_EQUAL_FUNCTION
+    | EN_FLOW_NOT_EQAUL_FUNCTION;
+type EN_FLOW_CODE_FUNCTION =
+    | EN_FLOW_EQUAL_FUNCTION
+    | EN_FLOW_NOT_EQAUL_FUNCTION;
+type EN_FLOW_STRING_FUNCTION =
+    | EN_FLOW_EQUAL_FUNCTION
+    | EN_FLOW_NOT_EQAUL_FUNCTION;
+type EN_FLOW_DATE_FUNCTION =
+    | EN_FLOW_EQUAL_FUNCTION
+    | EN_FLOW_NOT_EQAUL_FUNCTION;
+type EN_FLOW_DATETIME_FUNCTION =
+    | EN_FLOW_EQUAL_FUNCTION
+    | EN_FLOW_NOT_EQAUL_FUNCTION;
 
-type EN_FLOW_FUNCTION = EN_FLOW_DECIMAL_FUNCTION | EN_FLOW_CODE_FUNCTION | EN_FLOW_STRING_FUNCTION | EN_FLOW_DATE_FUNCTION | EN_FLOW_DATETIME_FUNCTION;
+type EN_FLOW_FUNCTION =
+    | EN_FLOW_DECIMAL_FUNCTION
+    | EN_FLOW_CODE_FUNCTION
+    | EN_FLOW_STRING_FUNCTION
+    | EN_FLOW_DATE_FUNCTION
+    | EN_FLOW_DATETIME_FUNCTION;
 
 enum EN_FLOW_RETURNTYPE {
     boolean = 'boolean',
-    decimal ='decimal',
-    string = 'string,
+    decimal = 'decimal',
+    string = 'string',
     date = 'date',
-    datetime = 'datetime'
+    datetime = 'datetime',
 }
 
 const LOGICAL_OPERATORS = {
-  AND: '&&' as const,
-  OR: '||' as const,
+    AND: '&&' as const,
+    OR: '||' as const,
 } as const;
 
 const ARITHMETIC_OPERATORS = {
-  MULTIPLY: '*' as const,
-  DIVIDE: '/' as const,
-  PLUS: '+' as const,
-  MINUS: '-' as const,
+    MULTIPLY: '*' as const,
+    DIVIDE: '/' as const,
+    PLUS: '+' as const,
+    MINUS: '-' as const,
 } as const;
 
 const COMPARER_OPERATORS = {
-  EQUAL: '=' as const,
+    EQUAL: '=' as const,
 } as const;
 
 // 각 연산자 그룹의 value들의 union 타입 추출
-type EN_LOGICAL_OPERATOR = typeof LOGICAL_OPERATORS[keyof typeof LOGICAL_OPERATORS];
-type EN_ARITHMETIC_OPERATOR = typeof ARITHMETIC_OPERATORS[keyof typeof ARITHMETIC_OPERATORS];
-type EN_COMPARER_OPERATOR = typeof COMPARER_OPERATORS[keyof typeof COMPARER_OPERATORS];
+type EN_LOGICAL_OPERATOR =
+    (typeof LOGICAL_OPERATORS)[keyof typeof LOGICAL_OPERATORS];
+type EN_ARITHMETIC_OPERATOR =
+    (typeof ARITHMETIC_OPERATORS)[keyof typeof ARITHMETIC_OPERATORS];
+type EN_COMPARER_OPERATOR =
+    (typeof COMPARER_OPERATORS)[keyof typeof COMPARER_OPERATORS];
 
 // 전체 연산자 타입
-type EN_OPERATOR_TYPE = EN_LOGICAL_OPERATOR | EN_ARITHMETIC_OPERATOR | EN_COMPARER_OPERATOR;
+type EN_OPERATOR_TYPE =
+    | EN_LOGICAL_OPERATOR
+    | EN_ARITHMETIC_OPERATOR
+    | EN_COMPARER_OPERATOR;
 
 // precedence와 operator를 맵핑
 const OPERATOR_PRECEDENCE: Record<EN_OPERATOR_TYPE, number> = {
-  [LOGICAL_OPERATORS.AND]: 1,
-  [LOGICAL_OPERATORS.OR]: 1,
-  [COMPARER_OPERATORS.EQUAL]: 2,
-  [ARITHMETIC_OPERATORS.MULTIPLY]: 3,
-  [ARITHMETIC_OPERATORS.DIVIDE]: 3,  
-  [ARITHMETIC_OPERATORS.PLUS]: 4,
-  [ARITHMETIC_OPERATORS.MINUS]: 4
+    [LOGICAL_OPERATORS.AND]: 1,
+    [LOGICAL_OPERATORS.OR]: 1,
+    [COMPARER_OPERATORS.EQUAL]: 2,
+    [ARITHMETIC_OPERATORS.MULTIPLY]: 3,
+    [ARITHMETIC_OPERATORS.DIVIDE]: 3,
+    [ARITHMETIC_OPERATORS.PLUS]: 4,
+    [ARITHMETIC_OPERATORS.MINUS]: 4,
 } as const;
 
 function operatorPrecedence(operator: EN_OPERATOR_TYPE): number {
-  const precedence = OPERATOR_PRECEDENCE[operator];
-  
-  if (precedence === undefined) {
-    throw new Error(`not found operator`);
-  }
-  
-  return precedence;
+    const precedence = OPERATOR_PRECEDENCE[operator];
+
+    if (precedence === undefined) {
+        throw new Error(`not found operator`);
+    }
+
+    return precedence;
 }
 
 enum EN_FLOW_VALUE_TYPE {
     String = 'string',
-    Decimal = 'decimal,
+    Decimal = 'decimal',
     Any = 'any',
     Date = 'date',
     DateTime = 'datetime',
@@ -127,9 +154,9 @@ enum EN_FLOW_VALUE_TYPE {
 
 interface IFlowAst {
     /*
-     * 각 node 의 sequence id
-     * sid rule: EN_FLOW_TYPE + '_' + 난수(8자리)
-     */
+    * 각 node 의 sequence id
+    sid rule: EN*FLOW_TYPE + '*' + 난수(8자리)
+    */
     sid: string;
 }
 
@@ -152,7 +179,7 @@ interface IFlowAstProgram extends IFlowAst {
  */
 interface IFlowAstMetadataStatement extends IFlowAstStatement {
     type: EN_FLOW_AST.MetadataStatement;
-    statement: IFlowAstStatement
+    statement: IFlowAstStatement;
 }
 
 /*
@@ -166,7 +193,7 @@ interface IFlowAstIfStatement extends IFlowAstStatement {
 
 /*
  * 결과를 나타내기 위해 사용
- * 단, EN_FLOW_VALUE_TYPE 이 적용되지 않는 case 에 사용 
+ * 단, EN_FLOW_VALUE_TYPE 이 적용되지 않는 case 에 사용
  * ex) expressions 에 명시되는 값은 모두 문자열 취급된다.
  */
 interface IFlowAstCombinationStatement extends IFlowAstStatement {
@@ -179,17 +206,18 @@ interface IFlowAstCombinationStatement extends IFlowAstStatement {
  */
 interface IFlowAstResultStatement extends IFlowAstStatement {
     type: EN_FLOW_AST.ResultStatement;
-    value: IFlowAstExpression
+    value: IFlowAstExpression;
+    value_type: EN_FLOW_VALUE_TYPE;
 }
 
 //expression
-interface IFlowAstBinaryExpression exnteds IFlowAstExpression {
+interface IFlowAstBinaryExpression extends IFlowAstExpression {
     type: EN_FLOW_AST.BinaryExpression;
     left: IFlowAstExpression;
     operator: EN_OPERATOR_TYPE;
     right: IFlowAstExpression;
 }
-interface IFlowAstCallExpression exnteds IFlowAstExpression {
+interface IFlowAstCallExpression extends IFlowAstExpression {
     type: EN_FLOW_AST.CallExpression;
     callee: `${EN_FLOW_UI_PROVIDER}.${EN_FLOW_FUNCTION}` | 'any';
     arguments: Record<string, any>;
@@ -201,12 +229,12 @@ interface IFlowAstArrayExpression extends IFlowAstExpression {
 }
 interface IFlowAstObjectExpression extends IFlowAstExpression {
     type: EN_FLOW_AST.ResultStatement;
-    value: IFlowAstPropertyExpression[];	
+    value: IFlowAstPropertyExpression[];
 }
 interface IFlowAstPropertyExpression extends IFlowAstExpression {
     type: EN_FLOW_AST.ResultStatement;
     key: IFlowAstLiteralExpression;
-    value: IFlowAstExpression
+    value: IFlowAstExpression;
 }
 interface IFlowAstLiteralExpression extends IFlowAstExpression {
     type: EN_FLOW_AST.ResultStatement;
@@ -219,5 +247,5 @@ interface IFlowAstOperatorExpression extends IFlowAstExpression {
 }
 interface IFlowAstBracketExpression extends IFlowAstExpression {
     type: EN_FLOW_AST.ResultStatement;
-    kind: EN_BRACKET_TYPE;	
+    kind: EN_BRACKET_TYPE;
 }
